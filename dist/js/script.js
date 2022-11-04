096392c3ef7bc2116f666afb711a8a6a3dcab89d
guilhermearaujoo <ghma.em1@gmail.com>
2022-11-04 01:41:03 -0300

    Add color changes when choses house

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,28 @@
 let divInput = document.querySelectorAll('.input');
 let input = document.querySelectorAll('.input-login')
+const buttonSendTop = document.querySelector('.head-login-button');
 
-
-input.forEach((element, index) => element.addEventListener('click', () => {
-    divInput.forEach(div => div.classList.remove('active'));
+//gives moviment for the input div
+input.forEach((element, index) => element.addEventListener('click', () => { 
     divInput[index].classList.add('active');
 }));
+
+input.forEach((element, index) => element.addEventListener('keypress', () => { 
+    divInput[index].classList.add('active');
+}));
+
+
+//validate de login information and throw and warning
+const validateLogin = () => {
+    (input[0].value === "tryber@teste.com" && input[1].value === "123456") ? window.alert("Olá, Tryber!") : window.alert("Email ou senha inválidos.");
+}
+buttonSendTop.addEventListener('click', validateLogin);
+
+const selected = document.getElementById('house');
+
+selected.addEventListener('change', () => {
+    const body = document.getElementsByTagName('body')[0];
+    body.className = selected.value;
+})
+
+
